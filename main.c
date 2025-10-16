07cdaf9f63546d3cba1a33b56cedb47c607aca83
kimmjuu <2014069@sookmyung.ac.kr>
2025-10-16 15:37:43 +0900

    07-3(1)

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
+int inc(int counter);
+
 int main(void)
 {
-    int i;
-    int temp = 0;
+    int i = 10;
 
-    for (i = 0; i < 5; i++)
-    {
-        temp++; 
-        printf("temp = %d\n", temp);
-    }
+    printf("i = %d\n", i);
+    inc(i);
+    printf("i = %d\n", i);
 
     return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
