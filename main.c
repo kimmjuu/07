9db3bc8d3c4c1e31995404303f92ba1b339e3d17
kimmjuu <2014069@sookmyung.ac.kr>
2025-10-16 15:54:08 +0900

    07-5

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-void f(void);
 
-int i;
-void f(void)
-{
-    for (int j = 0; j < 10; j++)
-        printf("#");
-    printf("\n");
-}
+void sub(void);
 
 int main(void)
 {
-    for (i = 0; i < 5; i++)
-        f();
+    int i;
+    for (i = 0; i < 3; i++)
+        sub();
     return 0;
+}
+
+void sub(void)
+{
+    int auto_count = 0;
+    static int static_count = 0;
+
+    auto_count++; 
+    static_count++; 
+
+    printf("auto_count = %d\n", auto_count);
+    printf("static_count = %d\n\n", static_count);
 }
